a8b90864966804701ef1e875cf0fad18e5cc01c5
Ronak Bansal <ronakbansal@gmail.com>
2017-12-20 13:45:35 +0530

    Debig

--- misc.go
+++ misc.go
@@ -136,6 +136,7 @@ func postForm(ctx context.Context, endpoint string, values url.Values, intf inte
 		return err
 	} else {
 		for resp != nil && resp.StatusCode == 429 {
+			logger.Printf("Endpoint breaking: %s\n", values["token"][0])
 			logResponse(resp, debug)
 			retryAfter := resp.Header.Get("Retry-After")
 			retryAfterSeconds, err := strconv.Atoi(retryAfter)
